a29a327505bd951b9793e7e76d7ca1c632a074d6
izban <izban@mail.ru>
2015-04-07 13:57:13 +0300

    jar

---
+++ getJarImplementor.sh
@@ -0,0 +1,9 @@
+cd out/production/java-advanced-2015
+jar cvfm Implementor.jar ../../../META-INF/MANIFEST.MF ./ru/ifmo/ctddev/zban/implementor/*.class
+#jar cvfm ../../../artifacts/Implementor.jar ../../../META-INF/MANIFEST.MF ./ru/ifmo/ctddev/zban/implementor/*.class
+
+#cd out/production/java-advanced-2015/ru/ifmo/ctddev/zban/implementor
+#jar cmf ../../../../../../../../META-INF/MANIFEST.MF Implementor.jar *.class
+
+#cd out/production/java-advanced-2015
+#jar cmf ../../../META-INF/MANIFEST.MF ru/ifmo/ctddev/zban/implementor/Implementor.jar ru/ifmo/ctddev/zban/implementor/*.class
