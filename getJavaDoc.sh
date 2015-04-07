2158935133860a8045c6d270480ae72cd9a9803c
izban <izban@mail.ru>
2015-04-07 20:02:53 +0300

    javadoc gen script

---
+++ getJavaDoc.sh
@@ -0,0 +1,4 @@
+rm -rf javadoc
+mkdir javadoc
+
+javadoc -d javadoc -link http://docs.oracle.com/javase/8/docs/api/ -cp java:lib/hamcrest-core-1.3.jar:lib/junit-4.11.jar:lib/quickcheck-0.6.jar: `find java/ru/ifmo/ctddev/zban -iname '*.java'`
